4c69f20006d555e86eec10d49c756a6120ea4a7a
marco <marco@marco-pc.corp.hailtek.com>
2015-05-07 09:53:19 -0500

    aumentando funcionalidad del cliente

--- src/practica/main/Ntp_Client.java
+++ src/practica/main/Ntp_Client.java
@@ -5,9 +5,11 @@
  */
 package practica.main;
 
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +25,7 @@ public class Ntp_Client extends Thread {
     static Double[] latencia = new Double[8];
     static Double[] compensacion = new Double[8];
     static Double[][] tab = new Double[8][2];
+    static String T1server, T2server;
     private static ArrayList<Tiempo> tiempos= new ArrayList<>();
 
     public static void main(String[] args) throws InterruptedException {
@@ -40,7 +43,7 @@ public class Ntp_Client extends Thread {
             }
         }
         
-        actualiza(com);
+        actualiza(com,T2server);
         //System.out.println("lat es: "+lat+" y com es: "+com);
 
     }//end main 
@@ -49,13 +52,13 @@ public class Ntp_Client extends Thread {
 
         DataOutputStream send;
         DataInputStream receive;
-        String T1server, T2server;
+        
         double A, B, C;
         int cont = 0;
         
        // while (cont < 8) {
             try {
-                Socket socket = new Socket("10.20.3.181", 5000);
+                Socket socket = new Socket("127.0.0.1", 5000);
                 send = new DataOutputStream(socket.getOutputStream());
                 Date tiempo1 = new Date();//hora cliente
                 String envio = Long.toString(tiempo1.getTime()); //convertimos a cadena 
@@ -68,11 +71,9 @@ public class Ntp_Client extends Thread {
                 Date tiempo2 = new Date();//hora cliente
                 //obtenemos las horas
                 String[] palabras = horaS.split("@");
-                // for (String palabra:palabras) 
+
                 T1server = palabras[0]; //tiempo de recibo
                 T2server = palabras[1]; //tiempo de envio
-                // System.out.println (T1server);
-                //System.out.println (T2server);
 
                 A = tiempo1.getTime() - Double.parseDouble(T1server);  // T2-T1
                 B = Double.parseDouble(T2server) - tiempo2.getTime();  // T3-T4
@@ -87,9 +88,6 @@ public class Ntp_Client extends Thread {
                 tab[cont][0] = latencia[cont]; // creamos una matriz
                 tab[cont][1] = compensacion[cont];
 
-                //System.out.println("lat: " + Latencia[cont]); 
-                //System.out.println("Com: " + Compensacion[cont]);
-
                 socket.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
@@ -99,30 +97,26 @@ public class Ntp_Client extends Thread {
        // Actualiza();
     }//end Run
 
-    public static void actualiza(long compensacion){
-              
-        /*Arrays.sort(tab);// ordenamos el arrays de forma ascendente
-        //imprimimos la tab
-        for(int f=0;f<tab.length;f++) {
-            for(int c=0;c<tab[f].length;c++) {
-                System.out.print(tab[f][c]+" ");
-            }
-            System.out.println();
-        }*/
-        //obtenemos la min latecia y compensacion [0]
-        //Arrays.sort(Latencia);// ordenamos el arrays de forma ascendente
-        //Arrays.sort(Compensacion);// ordenamos el arrays de forma ascendente
-        //mostramos el arrays
-        // for (Double lat : Latencia) { 
-        //    System.out.println("lat: " + lat);
-        //}
-        Date T =new Date(); //obtenemos la fecha
-        Long Hora = T.getTime() + compensacion; //sumamos la compensacion a  la fecha
-
+    public static void actualiza(long compensacion,String T2server){
+     
+        Long Hora = Long.parseLong(T2server) + compensacion; //sumamos la compensacion a  la fecha
+        System.out.println("la compensacion es: "+compensacion);
         String H =Long.toString(Hora);
         
         try{
-        Runtime.getRuntime().exec("date --set '"+ H+"'"); //actualizamos la fecha
+            Date fecha = new Date(Hora);
+            System.out.println("la hora que asigno es: "+fecha.toString());
+            //Runtime.getRuntime().exec("sudo su");
+            //Runtime.getRuntime().exec("Marco18346839");
+            String array[]={"sudo","-S","su"};
+            String pass ="Marco18346839";
+            Process proc = null;
+            BufferedWriter writer = null;
+            proc = Runtime.getRuntime().exec(array);
+            writer = new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
+            writer.write(pass);
+            
+            Runtime.getRuntime().exec("date --set '"+fecha+"'"); //actualizamos la fecha
         
         System.out.println(" Establecido correctamente \n");
         }
